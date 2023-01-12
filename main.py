d3819072a869fdeeff3c7237bd9eafcd38f4c813
NaDisla <tejadanadiam@gmail.com>
2023-01-12 19:19:43 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Comentario de una línea
+"""
+Comentario multilínea
+"""
+suma = 52 # Variables de tipo entero
+decimal = 45.20 # Variable de tipo decimal
+bool_variable = True #Variable de tipo boolean
+bool_variable_02 = False #Variable de tipo boolean
+string = "Cadena" # Cadena de caracteres
+dictionary = {
+    "Nombre": "Juan",
+    "Edad": 30,
+    "Estudiante": False
+} # Diccionario
+lista = ["Hola", 34, 52.02] # Lista
