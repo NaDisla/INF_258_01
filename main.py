4579b19b0fbb75f1f6e07c66f2dfa8aad3343b1d
Edu-B <eduardob@tagshelf.com>
2023-01-12 19:23:28 -0400

    add name

--- main.py
+++ main.py
@@ -2,14 +2,12 @@
 """
 Comentario multilínea
 """
-suma = 52 # Variables de tipo entero
-decimal = 45.20 # Variable de tipo decimal
-bool_variable = True #Variable de tipo boolean
-bool_variable_02 = False #Variable de tipo boolean
-string = "Cadena" # Cadena de caracteres
-dictionary = {
-    "Nombre": "Juan",
-    "Edad": 30,
-    "Estudiante": False
-} # Diccionario
-lista = ["Hola", 34, 52.02] # Lista
+suma = 52  # Variables de tipo entero
+decimal = 45.20  # Variable de tipo decimal
+bool_variable = True  # Variable de tipo boolean
+bool_variable_02 = False  # Variable de tipo boolean
+string = "Cadena"  # Cadena de caracteres
+dictionary = {"Nombre": "Juan", "Edad": 30, "Estudiante": False}  # Diccionario
+lista = ["Hola", 34, 52.02]  # Lista
+
+# Eduardo Artruo Batista Diaz-Valdepares
